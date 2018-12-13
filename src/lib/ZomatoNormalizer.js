fc8d0dd93e3e0cf0642cf4a828d6cd8c7233fe59
misok <miso.kucera97@gmail.com>
2018-12-13 20:04:32 +0100

    stop words removed and multilines merged in zomato response

--- src/lib/ZomatoNormalizer.js
+++ src/lib/ZomatoNormalizer.js
@@ -1,9 +1,30 @@
+const stopWords = [
+    '^POLÉVKA$',
+    'HLAVNÍ JÍDLA',
+    'DNES PO DOBU',
+    'S MOŽNOSTÍ ZAKOUPENÍ',
+    'SALÁTY a MALÁ LEHKÁ JÍDLA',
+    'Informaci o alergenech',
+    'Dle denní nabídky',
+    '^menu$',
+    '^SALÁT$',
+    '^SPECIALITA$',
+    '^Specialita dne$',
+    '^DEZERT$',
+    '^HLAVNÍ CHOD$',
+    '^Bezmasé jídlo$',
+    '^Stálá nabídka$',
+    '^NÁPOJ K POLEDNÍMU MENU$'
+];
+
 class ZomatoNormalizer {
 
     static normalizeDailyMenu(data) {
         const dishesPerMenu = data.daily_menus.map(item => item.daily_menu.dishes.map(ZomatoNormalizer.normalizeDish));
+        let dishes = dishesPerMenu.reduce((acc, value) => acc.concat(value), []);
 
-        const dishes = dishesPerMenu.reduce((acc, value) => acc.concat(value), []);
+        dishes = ZomatoNormalizer.removeStopWords(dishes, stopWords);
+        dishes = ZomatoNormalizer.mergeMultiLineDishes(dishes);
 
         return {
             updatedTime: Date.now(),
@@ -17,6 +38,20 @@ class ZomatoNormalizer {
             name: data.dish.name,
             price: data.dish.price
         }
+    }
+
+    static removeStopWords(dishes, stopWords) {
+        const regex = new RegExp(stopWords.join('|'), 'i');
+        return dishes.filter(dish => !dish.name.match(regex));
+    }
+
+    static mergeMultiLineDishes(dishes) {
+        dishes.forEach((dish, index) => {
+            if (!dish.price && dishes[index+1]) {
+                dishes[index+1].name = dish.name + ' ' + dishes[index+1].name;
+            }
+        });
+        return dishes.filter(dish => dish.price);
     }
 
 }
