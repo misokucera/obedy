eaee163d537ff63217f49df8790454e62dbe67f8
misok <miso.kucera97@gmail.com>
2019-01-09 23:57:27 +0100

    optimized firebase imports

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import firebase from "firebase";
+import firebase from "firebase/app";
 import firebaseConfig from "./firebase.config";
 
 firebase.initializeApp(firebaseConfig);
